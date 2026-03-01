f49f3812baeecd94186eff396aaa439403614890
oscar-dev <oclemente.dscont@gmail.com>
2026-02-28 22:32:13 -0500

    28022026 agregando mensaje inicial para saber en que puerto esta corriendo el backend

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule,{
+  //   logger: ['error', 'warn'],
+  // });
 
   const config = new DocumentBuilder()
     .setTitle('JuntasCloud API')
@@ -18,5 +22,7 @@ async function bootstrap() {
   app.enableCors();
 
   await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`Server running on ${await app.getUrl()}`, 'Bootstrap');
+
 }
 bootstrap();
